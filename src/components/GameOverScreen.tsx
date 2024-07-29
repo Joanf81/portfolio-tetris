4dd26e025614f90bd2c59dc794efd1ec07439b12
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-29 16:41:39 +0200

    Added game context to screens

--- src/components/GameOverScreen.tsx
+++ src/components/GameOverScreen.tsx
@@ -1,24 +1,20 @@
+import { useContext } from "react";
 import { log } from "../log.js";
+import { GameContext } from "../store/GameContext.js";
 
-interface GameOverScreenProps {
-  show: boolean;
-  restartGame(): void;
-}
+export default function GameOverScreen() {
+  const gameContext = useContext(GameContext);
 
-export default function GameOverScreen({
-  show,
-  restartGame,
-}: GameOverScreenProps) {
+  if (gameContext.gameState != "GAME_OVER") return;
+
   return (
     <div
-      className={`z-40 absolute ${
-        show ? "flex" : "hidden"
-      } flex-col justify-center items-center space-y-12 w-full h-full bg-gray-600/70 `}
+      className={`z-40 absolute flex flex-col justify-center items-center space-y-12 w-full h-full bg-gray-600/70 `}
     >
       <p className="text-5xl text-gray-100">Game over!</p>
       <button
         className="px-10 py-4 text-gray-100 text-2xl border hover:bg-gray-600/70"
-        onClick={restartGame}
+        onClick={gameContext.startGame}
       >
         Play again
       </button>
--- src/components/PausedScreen.tsx
+++ src/components/PausedScreen.tsx
@@ -1,21 +1,20 @@
+import { useContext } from "react";
 import { log } from "../log.js";
+import { GameContext } from "../store/GameContext.js";
 
-interface PausedScreenProps {
-  show: boolean;
-  resumeGame(): void;
-}
+export default function PausedScreen() {
+  const gameContext = useContext(GameContext);
 
-export default function PausedScreen({ show, resumeGame }: PausedScreenProps) {
+  if (gameContext.gameState != "PAUSED") return;
+
   return (
     <div
-      className={`z-40 absolute ${
-        show ? "flex" : "hidden"
-      } flex-col justify-center items-center space-y-12 w-full h-full bg-gray-600/70 `}
+      className={`z-40 absolute flex flex-col justify-center items-center space-y-12 w-full h-full bg-gray-600/70 `}
     >
       <p className="text-5xl text-gray-100">Game paused</p>
       <button
         className="px-10 py-4 text-gray-100 text-2xl border hover:bg-gray-600/70"
-        onClick={resumeGame}
+        onClick={gameContext.pauseGame}
       >
         Resume
       </button>
