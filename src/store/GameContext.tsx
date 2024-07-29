ee86ff869ffa800b376be11c5cbd5c3e5894c439
Joan Fernández Bornay <joanf81@gmail.com>
2024-07-29 14:43:40 +0200

    Splitted game context and game reducer

--- src/store/GameContext.tsx
+++ src/store/GameContext.tsx
@@ -1,18 +1,14 @@
 import { PropsWithChildren, createContext, useEffect, useReducer } from "react";
-import { boardColsNumber } from "../config";
-import { PieceMap, PiecePositionZType, nextPositionZ } from "../lib/piece";
+
+import { gameReducer } from "./GameReducer";
+
+import { BoardType, createEmptyBoard } from "../lib/board";
 import {
-  isCollisionAgainstBoardLimit as boardCollision,
-  isCollisionAgainstPiece as pieceCollision,
-} from "../lib/collisions";
-import {
-  BoardType,
-  addPieceToBoard,
-  copyBoard,
-  createEmptyBoard,
-  emptyBoardLine,
-} from "../lib/board";
-import { ActivePiece, getResetedActivePiece } from "../lib/piece";
+  ActivePiece,
+  getResetedActivePiece,
+  PieceMap,
+  PiecePositionZType,
+} from "../lib/piece";
 
 type GameState = "INITIAL" | "RUNNING" | "PAUSED" | "GAME OVER";
 export interface GameContextType {
@@ -46,122 +42,6 @@ const initialGameContext: GameContextType = {
 };
 
 export const GameContext = createContext<GameContextType>(initialGameContext);
-
-type setGameStateRunning = { type: "SET_RUNNING" };
-type emptyLinesFromBoardAction = {
-  type: "EMPTY_LINE";
-  payload: { line: number };
-};
-type movePieceRightAction = {
-  type: "MOVE_RIGHT";
-};
-type movePieceLeftAction = { type: "MOVE_LEFT" };
-type movePieceDownAction = {
-  type: "MOVE_DOWN";
-};
-type rotatePieceAction = { type: "ROTATE" };
-type restartPieceAction = { type: "RESTART" };
-
-type gameActionType =
-  | setGameStateRunning
-  | emptyLinesFromBoardAction
-  | movePieceRightAction
-  | movePieceLeftAction
-  | rotatePieceAction
-  | movePieceDownAction
-  | restartPieceAction;
-
-function gameReducer(state: GameContextType, action: gameActionType) {
-  const { board, activePiece } = state;
-  const { maps, positionX: X, positionY: Y, positionZ: Z } = activePiece;
-
-  switch (action.type) {
-    // case "EMPTY_BOARD":
-    //   board = createEmptyBoard();
-    //   break;
-
-    case "EMPTY_LINE":
-      const boardCopy = copyBoard(state.board);
-      const { line } = action.payload;
-
-      boardCopy.splice(line, 1);
-      boardCopy.splice(1, 0, emptyBoardLine);
-
-      return { ...state, board: boardCopy };
-
-    case "MOVE_RIGHT":
-      if (
-        X + maps[Z][0].length < boardColsNumber - 1 &&
-        !pieceCollision(board, maps[Z], X, Y, { incrementX: 1 })
-      ) {
-        return { ...state, activePiece: { ...activePiece, positionX: X + 1 } };
-      }
-      break;
-
-    case "MOVE_LEFT":
-      if (X > 1 && !pieceCollision(board, maps[Z], X, Y, { incrementX: -1 })) {
-        return { ...state, activePiece: { ...activePiece, positionX: X - 1 } };
-      }
-      break;
-
-    case "MOVE_DOWN":
-      if (
-        boardCollision(maps[Z], Y) ||
-        pieceCollision(board, maps[Z], X, Y, { incrementY: 1 })
-      ) {
-        // Collision against top limit
-        if (Y <= 1) {
-          // setGameState("GAME OVER");
-        } else {
-          return {
-            ...state,
-            board: addPieceToBoard(activePiece, board),
-            activePiece: getResetedActivePiece(),
-          };
-        }
-      } else {
-        return { ...state, activePiece: { ...activePiece, positionY: Y + 1 } };
-      }
-      break;
-
-    case "ROTATE":
-      const nextMap: PieceMap = maps[nextPositionZ(Z)];
-      const borderDistance = boardColsNumber - 1 - (X + nextMap[0].length);
-
-      // If there is no collision against right border and against any piece
-      if (borderDistance >= 0 && !pieceCollision(board, nextMap, X, Y, {})) {
-        return {
-          ...state,
-          activePiece: { ...activePiece, positionZ: nextPositionZ(Z) },
-        };
-      } else if (borderDistance < 0) {
-        if (
-          !pieceCollision(board, nextMap, X, Y, { incrementX: borderDistance })
-        ) {
-          return {
-            ...state,
-            activePiece: {
-              ...activePiece,
-              positionX: X + borderDistance,
-              positionZ: nextPositionZ(Z),
-            },
-          };
-        }
-      }
-      break;
-
-    case "RESTART":
-      return { ...state, activePiece: getResetedActivePiece() };
-  }
-  // case "SET_RUNNING":
-  //   if (state.state == "GAME OVER") {
-  //     boardContext.emptyBoard();
-  //     activePieceContext.restart();
-  //   }
-  //   break;
-
-  return state;
-}
 
 export default function GameContextProvider({ children }: PropsWithChildren) {
   const [gameState, gameDispatch] = useReducer(gameReducer, {
